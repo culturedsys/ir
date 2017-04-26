f34eec19308bba724f7600684b8cb91c33ea7b9b
Tim Fisken <contact@timfisken.org>
2017-04-26 16:12:28 +0100

    Move FrequencyTable into a separate file
    The Naive Bayes classifier will also use this class, so it should be in
    a separate file.

--- unigram.py
+++ unigram.py
@@ -4,23 +4,7 @@ Search using unigram language models
 
 import collections
 import operator
-
-
-class FrequencyTable(collections.defaultdict):
-    """
-    A table mapping tokens to frequency counts.
-    Also maintains the property `length`, the total length of the underlying document
-    """
-
-    def __init__(self, document):
-        collections.defaultdict.__init__(self, int)
-        length = 0
-
-        for token in document:
-            self[token] += 1
-            length += 1
-
-        self.length = length
+from frequencies import FrequencyTable
 
 
 def create_model(frequencies):
